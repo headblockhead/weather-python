df7b379f1df14e8d7eca7a4e814cd888898f5416
headblockhead <26520767+headblockhead@users.noreply.github.com>
2021-02-06 15:14:26 +0000

    added api key

--- mian.py
+++ mian.py
@@ -1,4 +1,4 @@
 from pprint import pprint
 import requests
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=London&APPID={APIKEYHERE}')
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=d6942ef68c1f871235cc674aaf71ff46')
 pprint(r.json())
